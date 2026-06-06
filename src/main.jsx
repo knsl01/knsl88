290ff611629b76f02b5c82f6dd144838a0992d83
knsl01 <139193077+knsl01@users.noreply.github.com>
2026-06-07 00:34:42 +0700

    Update and rename index.js to main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import KNSLLegalIntelligence from './KNSLLegalIntelligence'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KNSLLegalIntelligence />
+  </React.StrictMode>,
+)
