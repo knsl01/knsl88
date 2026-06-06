0da59f692e869e8a136e19b3b31dd333f688d2df
knsl01 <139193077+knsl01@users.noreply.github.com>
2026-06-07 00:34:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // kalau deploy ke GitHub Pages di sub-path, set base: "/nama-repo/"
-  base: "./",
-});
+})
